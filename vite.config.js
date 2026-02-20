1bd7bbb915e65506c8cc5622c62a62681ba8c555
Loki-of-Norway <thekva@krokeide.vgs.no>
2026-02-20 01:59:57 +0100

    Rename to MidtunDekk

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MittunDekk/',
+  base: '/MidtunDekk/',
   plugins: [
     react(),
     tailwindcss(),
